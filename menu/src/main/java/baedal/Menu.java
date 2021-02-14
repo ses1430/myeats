e6636cf9e70f5248207cd1e3ab57e6a6e88c9164
Im Hyeong-seob <ses1430@gmail.com>
2021-02-14 21:29:34 +0900

    .

---
+++ menu/src/main/java/baedal/Menu.java
@@ -0,0 +1,49 @@
+package baedal;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Menu_table")
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String menuNm;
+
+    @PostPersist
+    public void onPostPersist(){
+        MenuCreated menuCreated = new MenuCreated();
+        BeanUtils.copyProperties(this, menuCreated);
+        menuCreated.publishAfterCommit();
+
+
+        MenuDeleted menuDeleted = new MenuDeleted();
+        BeanUtils.copyProperties(this, menuDeleted);
+        menuDeleted.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getMenuNm() {
+        return menuNm;
+    }
+
+    public void setMenuNm(String menuNm) {
+        this.menuNm = menuNm;
+    }
+
+
+
+
+}
